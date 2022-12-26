c1475009dd7ec7d02b9ddb0abfd8e76d20c0f6dc
Aaron Barrett <abarrett471@gmail.com>
2022-12-26 01:29:46 -0600

    rename exercises

--- c_programming_language/1.5.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("To see that getchar() != EOF = 1, enter any character.\n");
-    printf("To see that getchar() != EOF = 0, enter CTRL-Z.\n");
-    int c = (getchar() != EOF);
-    printf("getchar() != EOF = %d", c);
-}
--- c_programming_language/1.6.c
+++ c_programming_language/1.6.c
@@ -2,6 +2,8 @@
 
 int main()
 {
-    printf("EOF= %d",EOF);
-    return 0;
+    printf("To see that getchar() != EOF = 1, enter any character.\n");
+    printf("To see that getchar() != EOF = 0, enter CTRL-Z.\n");
+    int c = (getchar() != EOF);
+    printf("getchar() != EOF = %d", c);
 }

24f3a8eae835315423a1842a2ac0ee37f8b8a293
Aaron Barrett <abarrett471@gmail.com>
2022-12-27 04:30:23 -0600

    1.20

---
+++ c_programming_language/chap_1/1.20.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define MAXLINE 1000
+#define TABSTOP 8
+
+int entab(char line[], int max);
+
+int main()
+{
+    char line[MAXLINE];
+    while ((entab(line, MAXLINE)) > 0)
+        printf("%s", line);
+
+    return 0;
+}
+
+int entab(char s[], int lim)
+{
+    int c, i,j;
+    int tab_count = 0;
+    for(i = 0 ; (i < lim-1) && (c=getchar()) != EOF && c != '\n' ; ++i)
+        if (c == '\t'){
+            tab_count = tab_count % TABSTOP;
+            tab_count = TABSTOP - tab_count;
+            for(; tab_count != 0; --tab_count){
+                s[i] = ' ';
+                if (tab_count != 1)
+                    ++i;
+            }
+        }
+        else{
+            ++tab_count;
+            s[i] = c;
+        }
+    if (c == '\n'){
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    if (c != '\n' && c != EOF)
+        while ((c=getchar()) != EOF && c != '\n')
+            ++i;
+    return i;
+}

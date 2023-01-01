a4f2cd9b464771f046329e051e02e0cc9b99dba4
Aaron Barrett <abarrett471@gmail.com>
2023-01-01 03:05:43 -0600

    2.6 finished exercise

--- c_programming_language/chap_2/2.6.c
+++ c_programming_language/chap_2/2.6.c
@@ -1,16 +1,68 @@
 #include <stdio.h>
+#include <limits.h>
+#include <string.h>
+
+#define MAXLINE 1000
+
+void reverse(char s[])
+{
+    int c, i, j;
+    for(i = 0, j = strlen(s)-1; i < j; i++, j--){
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+void itob(unsigned n, int b, char s[])
+{
+    if (b < 2 || b > 36){
+        printf("Base is out of range for base: %d", b);
+        return ;
+    }
+    int i,hold;
+    i = 0 ; 
+    do{
+        if (b <= 10)
+            s[i++] = n % b + '0';
+        else {
+            hold = n % b ;
+            if (hold >= 10)
+                s[i++] = (hold % 10) + 'A';
+            else 
+                s[i++] = hold + '0';
+        }
+    } while ((n /= b) > 0);
+    s[i++] = '\0';
+    reverse(s);
+}
 
 int setbits(unsigned x, int p, int n, unsigned y)
 {
-    return 0;
+    char s[MAXLINE];
+    unsigned z = (~(y >> (p-n+1)) & ~((~0 << n)));
+    z = z << (p-n+1);
+    y = (y >> (p-n+1)) & (~(~0 << n));
+    y = y << (p-n+1);
+    x = x | y ;
+    x = x ^ z;
+    return x;
 }
 
 int main()
 {
-    // unsigned x = 0b1110111; // 0b not necessarily in the standard
-    int x = 0b1110111;
+    // int x = 0b1110111; // 0b not necessarily in the standard
+    unsigned x = 0b1111101;
     int p = 4; 
-    int n = 3;
-    unsigned y = 0b1110111;
-    int result = setbits(x,p,n,y);
+    int n = 2;
+    unsigned y = 0b1110011;
+    unsigned result = setbits(x,p,n,y);
+    char s1[MAXLINE];
+    char s2[MAXLINE];
+    char s3[MAXLINE];
+    itob(x, 2, s1);
+    itob(y, 2, s2);
+    itob(result, 2, s3);
+    printf("x = %s\ty = %s \tresult = %s\n",s1,s2,s3);
+    return 0;
 }

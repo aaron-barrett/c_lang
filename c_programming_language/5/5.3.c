88173800845d6cc6c0fad2b89c722218e0bb3210
Aaron Barrett <abarrett471@gmail.com>
2023-01-07 01:58:22 -0600

    5.3 and 5.4

---
+++ c_programming_language/5/5.3.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+#define ALLOCSIZE 10             /* size of available space */
+
+static char allocbuf[ALLOCSIZE];    /* storage for alloc */
+static char* allocp = allocbuf;      /* next free position */
+
+char* alloc (int n) /* return pointer to n characters */
+{
+    if (allocbuf + ALLOCSIZE - allocp >= n){ /* it fits */
+        allocp += n;
+        return allocp - n; /* old p*/
+    }
+    else 
+        return 0;
+}
+
+void afree (char* p)                /* free storage pointed to by p */
+{
+    if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+        allocp = p;
+}
+
+void strcat_s(char* s, char* t)
+{
+    char* x = s;
+    char* y = t;
+    while (*++x != '\0');
+    while ((*x++ = *y++) != '\0');
+}
+
+int main()
+{
+    
+    char* s = alloc(5);
+    *s = '1';
+    *(s+1) = '2';
+    *(s+2) = '\0'; 
+    char* t = alloc(3);
+    *t = '3';
+    *(t+1) = '4';
+    *(t+2) = '\0'; 
+    printf("s = %s\n", s);
+    printf("t = %s\n", t);
+
+    printf("Full Buffer = ");
+    for(int i = 0 ; i < ALLOCSIZE; i++)
+        printf("%c ", allocbuf[i]);
+    printf("\n");
+
+    printf("s address = %d\n", s);
+    printf("t address = %d\n", t);
+    strcat_s(s,t);
+    printf("s = %s\n", s);
+    printf("t = %s\n", t);
+    printf("s address = %d\n", s);
+    printf("t address = %d\n", t);
+
+    printf("Full Buffer = ");
+    for(int i = 0 ; i < ALLOCSIZE; i++)
+        printf("%c ", allocbuf[i]);
+    printf("\n");
+    return 0;
+}
---
+++ c_programming_language/5/5.4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int strend(char* s, char* t)
+{
+    size_t size_sstr = 0;
+    size_t size_tstr = 0;
+    while(*++s != '\0')
+        size_sstr++;
+    while(*++t != '\0')
+        size_tstr++;
+    for(; size_tstr != -1; size_tstr--)
+        if (*--s != *--t)
+            return 0;
+    return 1;
+}
+
+int main()
+{
+    char* s = "First String";
+    char *t = "String";
+    char *u = "SString";
+    if (strend(s,t) == 1)
+        printf("\"%s\" appears at the end of \"%s\".\n", t,s);
+    else
+        printf("\"%s\" does not appears at the end of \"%s\".\n", t,s);
+
+
+    if (strend(s,u) == 1)
+        printf("\"%s\" appears at the end of \"%s\".\n", u,s);
+    else
+        printf("\"%s\" does not appears at the end of \"%s\".\n", u,s);
+    return 0;
+}

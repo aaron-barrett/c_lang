09aa5612bd45c7728443fb3673f9df439c5a708a
Aaron Barrett <abarrett471@gmail.com>
2023-01-07 02:01:01 -0600

    5.4 clenaup

--- c_programming_language/5/5.4.c
+++ c_programming_language/5/5.4.c
@@ -8,6 +8,8 @@ int strend(char* s, char* t)
         size_sstr++;
     while(*++t != '\0')
         size_tstr++;
+    if (size_sstr < size_tstr)
+            return 0;
     for(; size_tstr != -1; size_tstr--)
         if (*--s != *--t)
             return 0;
@@ -19,15 +21,16 @@ int main()
     char* s = "First String";
     char *t = "String";
     char *u = "SString";
+
     if (strend(s,t) == 1)
         printf("\"%s\" appears at the end of \"%s\".\n", t,s);
     else
         printf("\"%s\" does not appears at the end of \"%s\".\n", t,s);
 
-
     if (strend(s,u) == 1)
         printf("\"%s\" appears at the end of \"%s\".\n", u,s);
     else
         printf("\"%s\" does not appears at the end of \"%s\".\n", u,s);
+        
     return 0;
 }

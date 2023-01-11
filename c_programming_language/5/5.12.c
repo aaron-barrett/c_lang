3acc65072f7956c639cb3f2ece239092fa8524ec
Aaron Barrett <abarrett471@gmail.com>
2023-01-11 03:17:38 -0600

    5.12 almost done'

--- c_programming_language/5/5.12.c
+++ c_programming_language/5/5.12.c
@@ -3,38 +3,52 @@
 
 #define MAXLINE 1000
 
-int entab(char line[], int max, int tabstop);
-int detab(char line[], int max, int tabstop);
+int entab(char line[], int max, int tabstop, int column_start);
+int detab(char line[], int max, int tabstop, int column_start);
 int getlines(char s[], int lim);
 
 int main(int argc, char* argv[])
 {
-    int TABSTOP = 8;
-    int tab_mode = 0;
-    char s[MAXLINE];
-    if (argc != 3){
+    if (argc != 4){
         printf("Not enough arguments.\n");
         return 0;
     }
-    else{
-        TABSTOP = atoi(argv[1]);
-        tab_mode = atoi(argv[2]);
+    char s[MAXLINE];
+    long lineno = 0;
+    int c;
+    int TABSTOP = 8;
+    int column = 8;
+    int tab_mode = atoi(argv[3]);
+    while (argc > 0){
+        argv++;
+        argc--;
+        c = *argv[0];
+        switch(c){
+            case '-':
+                column = atoi(++argv[0]);
+                break;
+            case '+':
+                TABSTOP = atoi(++argv[0]);
+                break;
+            default:
+                argc = 0;
+                break;
+        }
     }
     switch(tab_mode){
         case 0 :
             printf("Enter a single line to entab:\n");
-            entab(s,MAXLINE,TABSTOP);
+            entab(s,MAXLINE,TABSTOP,column);
             printf("%s\n",s);
             break;
         case 1:
             printf("Enter a single line to detab:\n");
-            detab(s, MAXLINE, TABSTOP);
+            detab(s, MAXLINE, TABSTOP,column);
             printf("%s\n",s);
             break;
         default :
             printf("Invalid tab mode.\n");
             break;
-
     }
     return 0;
 }
@@ -53,7 +67,7 @@ int getlines(char s[], int lim)
     return i;
 }
 
-int detab(char s[], int lim, int TABSTOP)
+int detab(char s[], int lim, int TABSTOP, int column_start)
 {
     int c, i,j;
     int tab_count = 0;
@@ -82,7 +96,7 @@ int detab(char s[], int lim, int TABSTOP)
     return i;
 }
 
-int entab(char s[], int lim, int TABSTOP)
+int entab(char s[], int lim, int TABSTOP, int column_start)
 {
     int c, i;
     i = 0;

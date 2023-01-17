74502da68ea08f74603597c074466b78ffec8513
Aaron Barrett <abarrett471@gmail.com>
2023-01-16 23:42:30 -0600

    5.20 cleanup, comment logic, prettify output so just remove double spaces

--- c_programming_language/5/5.20.c
+++ c_programming_language/5/5.20.c
@@ -119,20 +119,25 @@ int gettoken(void) /* returns next token */
 void func_args(char* hold)
 {
     int counter = 0;
-    int type_type;
-    char hold_type[MAXARG][MAXTOKEN];
+    int type;
+    char hold_args[MAXARG][MAXTOKEN];
     for(int i = 0 ; i < MAXARG ; i++)
-        hold_type[i][0] = '\0';
+        hold_args[i][0] = '\0';
     if (tokentype == TYPE)
-        strcpy(hold_type[counter++], token);
-    while (gettoken() != ')'){
+        strcpy(hold_args[counter++], token);
+    while (gettoken() != ')')
         if (tokentype == '*')
-            strcpy(hold_type[counter++], " pointer to ");
-        else if (tokentype == QUALIFIER || tokentype == TYPE)
-            strcpy(hold_type[counter++], token);
+            strcpy(hold_args[counter++], "pointer to ");
+        else if (tokentype == QUALIFIER){
+            strcpy(hold_args[counter], token);
+            strcat(hold_args[counter++], " "); /* just prettifies output */
+
+        }
+        else if (tokentype == TYPE)
+            strcpy(hold_args[counter++], token);
         else if (tokentype == ','){
             while (counter > 0)
-                strcat(hold, hold_type[--counter]);
+                strcat(hold, hold_args[--counter]);
             strcat(hold, " and ");
         }
         else if (tokentype == '('){
@@ -140,27 +145,26 @@ void func_args(char* hold)
             hold_func[0] = '\0';
             char return_type[100];
             if (gettoken() == '*' && gettoken()  == NAME){
-                strcpy(return_type, hold_type[--counter]);
-                strcpy(hold_type[counter], "pointer to function ");
-                strcat(hold_type[counter], "\"");
-                strcat(hold_type[counter], token);
-                strcat(hold_type[counter], "\"");
-                if (gettoken() == ')' && (type_type = gettoken()) == '('){
-                    strcat(hold_type[counter], " passing ");
+                strcpy(return_type, hold_args[--counter]);
+                strcpy(hold_args[counter], "pointer to function ");
+                strcat(hold_args[counter], "\""); /* quotes prettify the function name*/
+                strcat(hold_args[counter], token);
+                strcat(hold_args[counter], "\"");
+                if (gettoken() == ')' && (type = gettoken()) == '('){
+                    strcat(hold_args[counter], " passing ");
                     func_args(hold_func);
-                    strcat(hold_type[counter], hold_func);
+                    strcat(hold_args[counter], hold_func);
                 }
-                else if (type_type == PARENS)
-                    strcat(hold_type[counter], hold_func);
-                strcat(hold_type[counter], " returning");
-                strcat(hold_type[counter++], return_type);
+                else if (type == PARENS)
+                    strcat(hold_args[counter], hold_func);
+                strcat(hold_args[counter], " returning");
+                strcat(hold_args[counter++], return_type);
             }
             else 
                 printf("Syntax error in function arguments.\n");
         }
-    }
     while (counter > 0)
-        strcat(hold, hold_type[--counter]);
+        strcat(hold, hold_args[--counter]);
 }
 
 /* dcl: parse a declarator */
@@ -171,7 +175,7 @@ void dcl(void)
         ns++;
     dirdcl();
     while(ns-- > 0)
-        if (strcmp(out, "") == 0)
+        if (strcmp(out, "") == 0) /* this just prettifies the output */
             strcat(out, "pointer to ");
         else
             strcat(out, " pointer to ");
@@ -190,25 +194,25 @@ void dirdcl(void)
     }
     else if (tokentype == NAME)
         strcpy(name, token);
-    else if (tokentype == TYPE ){ /*this does not handle the initial return type which is different */
+    else if (tokentype == TYPE ){ /*this does not handle the initial return type which is different; this is used ONLY for function arguments.*/
         func_args(argument);
         dirdcl();
     }
-    else if (tokentype == QUALIFIER){  /* only used for qualifiers for main type */
+    else if (tokentype == QUALIFIER){  /* only used for qualifiers for output type */
         char datatype_hold[10];
         strcpy(datatype_hold,token);
         strcat(datatype_hold," ");
         dcl();
         strcat(out, datatype_hold);
     }
-    else if (tokentype == ')'){
+    else if (tokentype == ')'){ /* bookends the call which detects function arguements, i.e., this breaks out of detecting function arguments for good.*/
         if (strcmp(argument, "\0") != 0){
             strcat(out, "function passing ");
             strcat(out, argument);
             argument[0] = '\0';
             strcat(out, " returning");
         }
-        return;
+        return; /* this seems spurious: this is the true bookend, essentially allowing us to resuse the exact functionality without function arguments.*/
     }
     else {
         printf("error :expected name or (dcl)\n");
@@ -216,16 +220,17 @@ void dirdcl(void)
     }
     if (tokentype != '\n')
         while ((type = gettoken())  == PARENS || type == BRACKETS)
-            if (type == PARENS){
+            if (type == PARENS)
                 strcat(out, "function returning");
-            }
             else if (type == BRACKETS) {
                 strcat(out, "array");
                 strcat(out, token);
                 strcat(out, " of");
             }
-    if (type == '(')
-        dcl();
+    if (type == '('){ /* initiall detects function arguments.*/
+        gettoken(); /* in this case, we merely needed to call gettoken() again; dcl() can replace gettoken() & dirdcl(), but checking for pointers here is unnecessary with out input assumptions */
+        dirdcl();
+    }
 }
 
 #define BUFSIZE 100

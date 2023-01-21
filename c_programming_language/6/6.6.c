70bd25fb6ddad9139baa1be46bd63a297a9f1e01
Aaron Barrett <abarrett471@gmail.com>
2023-01-21 05:49:48 -0600

    6.6 complete, chap 6 sans comments finished

--- c_programming_language/6/6.6.c
+++ c_programming_language/6/6.6.c
@@ -3,31 +3,41 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+#define HASHSIZE 101
+#define MAXWORD 100
 
 struct nlist{               /* table entry */
     struct nlist * next;    /* next entry in chain */
     char* name;             /* defined name */
     char* defn;             /* replacement text */
 };
-#define HASHSIZE 101
 
 static struct nlist* hashtab[HASHSIZE]; /* pointer table */
-
 unsigned hash(char* s);
 struct nlist* lookup(char* s);
 struct nlist* install(char* name, char* defn);
 void undef (char* name, char* defn);
 char* strdup_(char*);
 
-#define MAXWORD 100
+enum{NAME, DEFINITION};
 
+/* Possible example
+    #include <stdio.h>
+    #define VALUE 100
+    int array[VALUE];
+    int main()
+    {
+        for(int i = 0 ; i<VALUE ; i ++)
+            array[i] = VALUE * 500 - 34;
+        return 0;
+    }
+*/
 
 int main()
 {
-    int c, getch(void);
+    int c, type,  getch(void);
     void ungetch(int);
     char word[MAXWORD];
-    word[0] = '\0';
     int word_count = 0;
     char name[MAXWORD];
     char defn[MAXWORD];
@@ -35,76 +45,49 @@ int main()
     document[0] = '\0';
     int document_count = 0;
     struct nlist* np;
+
+    /* first call to ensure the main loop starts with an alphanumeric character or # */
+    while ( !isalnum(c = getch()) && c != '#')
+        document[document_count++] = c;
+    document[document_count] = '\0';
+    ungetch(c); /* avoids off by one error for the following loop */
     while ((c = getch()) != EOF){
-        printf("c = %c\n",c);
-        document_count = strlen(document);
-        while(!isalnum(c) && c != '#'){
+        if ( !isalnum(c) && c != '#'){
+            word[word_count] = '\0';
+            if (strcmp(word, "") != 0){
+                if (strcmp(word, "#define") == 0){
+                    type = NAME;
+                    strcat(document, word);
+                }
+                else if (type == NAME){
+                    strcpy(name, word);
+                    strcat(document, word);
+                    type = DEFINITION;
+                }
+                else if (type == DEFINITION)
+                {
+                    strcat(document, word);
+                    install(name, word);
+                    type = -1;
+                }
+                else if (((np = lookup(word) ) != NULL)){
+                    strcat(document, np->defn);
+                    np = NULL;
+                }
+                else
+                    strcat(document, word);
+                word[0] = '\0'; /* reset word for next alpha numeric charcter string (including #)*/
+                word_count = 0;
+            }
+            document_count = strlen(document);
             document[document_count++] = c;
-            c = getch();
         }
-        document[document_count] = '\0';
-        while (isalnum(c) || c == '#'){
+        else 
             word[word_count++] = c;
-            c = getch();
-        }
-        word[word_count] = '\0';
-        if (strcmp(word, "#define") == 0){
-            strcat(document,word);
-            document_count = strlen(document);
-            word[0] = '\0';
-            word_count=0;
-            while(!isalnum(c)){
-                document[document_count++] = c;
-                c = getch();
-            }
-            document[document_count] = '\0';
-            while (isalnum(c)){
-                word[word_count++] = c;
-                c = getch();
-            }
-            word[word_count] = '\0';
-            strcat(document, word);
-            strcpy(name, word);
-            document_count = strlen(document);
-            // document[document_count] = '\0';
-            word[0] = '\0';
-            word_count=0;
-            while(!isalnum(c)){
-                document[document_count++] = c;
-                c = getch();
-            }
-            document[document_count] = '\0';
-            while (isalnum(c)){
-                word[word_count++] = c;
-                c = getch();
-            }
-            word[word_count] = '\0';
-            strcat(document, word);
-            // document_count = strlen(document);
-            // document[document_count++] = c;
-            // document[document_count] = '\0';
-            strcpy(defn, word);
-            install(name, defn);
-        }
-        else if ((np = lookup(word) ) != NULL){
-            strcat(document, np->defn);
-            // ungetch(c);
-            // document_count = strlen(document);
-            // document[document_count++] = c;
-            // document[document_count] = '\0';
-        }
-        else {
-            strcat(document,word);
-            // ungetch(c);
-            // document_count = strlen(document);
-            // document[document_count++] = c;
-            // document[document_count] = '\0';
-        }
-        // ungetch(c);
-        word[0] = '\0';
-        word_count=0;
     }
-    printf("%s\n", document);
+    document[document_count] = '\0';
+    printf("AFTER PREPROCESSING #DEFINE VALUES\n");
+    printf("%s", document);
     return 0;
 }
 
@@ -146,15 +129,6 @@ struct nlist* install(char* name, char* defn)
     if ((np->defn = strdup(defn)) == NULL)
         return NULL;
     return np;
-}
-
-/* undef: remove a name and definition in hashtab*/
-void undef (char* name, char* defn)
-{
-    struct nlist *np;
-    for(np = hashtab[hash(name)]; np!= NULL; np = np->next)
-        if (strcmp(defn,np->defn) == 0)
-            hashtab[hash(name)] = np->next;
 }
 
 char* strdup_(char* s)

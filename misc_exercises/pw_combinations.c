5542ace89ada26a1f4d6b9c0713d5213a203a922
Aaron Barrett <abarrett471@gmail.com>
2023-01-24 18:39:03 -0600

    pw_combinations comments

--- misc_exercises/pw_combinations.c
+++ misc_exercises/pw_combinations.c
@@ -1,7 +1,12 @@
+/*********************************************************************************************************************************************
+*   Prints the first "MAXPW" number of passwords from "list" of length "MAXLENGTH". An admissible pw is one which respects the ordering of 
+*   characters from "list", meaning that A > a > B > b ....
+*   An admissible pw for the "list_short" is "AAA", "AAa", "AAB", "AAb", "Aaa", ...
+*********************************************************************************************************************************************/
 #include <stdio.h>
 #include <string.h>
 
-#define MAXPW 52
+#define MAXPW 20
 #define MAXLENGTH 3
 
 void obtain_pw(char save_pw[MAXPW][MAXLENGTH+1], char* pw, char* list, int list_index, int pw_index, int* count){
@@ -27,11 +32,11 @@ int main()
     char save_pw[MAXPW][MAXLENGTH+1];
     char pw[MAXLENGTH+1];
     char list[] = {'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S' ,'s', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '\0'};
-    // char list[] = {'A', 'a', 'B', 'b', '\0'};
+    char list_short[] = {'A', 'a', 'B', 'b', '\0'};
     int list_index = 0;
     int pw_index = 0;
     int count  = 0;
-    obtain_pw(save_pw, pw, list,list_index, pw_index, &count);
+    obtain_pw(save_pw, pw, list_short,list_index, pw_index, &count);
     for(int i = 0 ; i < MAXPW; i++)
         printf("%s\n", save_pw[i]);
     return 0;

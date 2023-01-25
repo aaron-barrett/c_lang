6f0dbce0a52bc385d7697340626536c3e1bbfbb6
Aaron Barrett <abarrett471@gmail.com>
2023-01-25 01:21:55 -0600

    added comments in 5.17 and 2.9

--- c_programming_language/2_types_operators_expressions/2.9.c
+++ c_programming_language/2_types_operators_expressions/2.9.c
@@ -4,7 +4,7 @@
 *   2.9: In a two's complement number system, x &= (x-1) deletes the rightmost 1-bit in x. Explain why. Use this observation to write a faster
 *   version of bitcount.
 *   
-*   Notes:
+*   Notes: (x-1) and x differ by exactly one bit. &= stores x & (x-1) in x, which now has one less bit than that before. 
 *********************************************************************************************************************************************/
 #include <stdio.h>
 

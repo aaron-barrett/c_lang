fa48c305c94533eb59ad48c30ef10949cfb16193
Aaron Barrett <abarrett471@gmail.com>
2023-10-30 19:44:53 -0500

    macros archetpyes again

---
+++ misc_exercises/preprocessor/debug_pointer_print.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Ensures first if statement is called
+#define DEBUG "DEFINED"
+
+#ifdef DEBUG
+#define REPORT_PTR_VALUE(v) printf("Pointer \"%s\" points to %p\n", #v, v)
+#else 
+#define REPORT_PTR_VALUE(v)
+#endif
+
+void func(const int* a_passed_to_func)
+{
+    REPORT_PTR_VALUE(a_passed_to_func);
+}
+
+int main()
+{
+    int a = 0 ; 
+    func(&a);
+    return 0;
+}
